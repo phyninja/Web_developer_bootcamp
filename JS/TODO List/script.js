83f204c33f59950adcab49dba8b9687a44c4263f
phyninja <rabh0153@colorado.edu>
2018-03-17 17:07:20 -0600

    TODO list complete

--- JS/TODO List/script.js
+++ JS/TODO List/script.js
@@ -1,12 +1,34 @@
 document.getElementById("Start").onclick = function(){
 	var todoList = []
 
+	/* addToList takes the item to add as the input and adds it to the end of the todoList*/
 	function addToList(todo){
 		todoList.push(todo)
 	}
 
+
+	/* Prints the list in the following format:
+	************
+	index0: List item 0
+	index1: List item 1
+	.
+	.
+	.
+	************
+	 */
 	function getList(){
-		console.log(todoList)
+		console.log("************")
+		todoList.forEach(function(element, i){
+			console.log(i + ": " + element)
+		})
+		console.log("************")
+	}
+
+	function deleteFromList(index){
+		/*array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+		@returs array of deleted Items*/
+		var deletedItem = todoList.splice(index, 1)
+		console.log("Deleted item " + index + ": " + deletedItem[0])
 	}
 
 	while(true){
@@ -19,6 +41,10 @@ document.getElementById("Start").onclick = function(){
 		else if (input == "list"){
 			getList();
 			alert("Please see the console for the list")
+		}
+		else if (input == "delete"){
+			var indexToDelete = prompt("Please type the item number you want to delete:");
+			deleteFromList(indexToDelete);
 		}
 		else if (input == "quit"){
 			break;
